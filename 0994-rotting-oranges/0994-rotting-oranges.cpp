c6b5b56935c88ac82a4be7767f27fb47921a8dda
Maheshkadam06 <92226051+Maheshkadam06@users.noreply.github.com>
2023-08-04 16:03:35 +0530

    Time: 5 ms (75.99%), Space: 13 MB (68.96%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,60 @@
 class Solution {
 public:
+    int level[10][10];
     int vis[10][10];
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<vector<int>> level(grid.size(),vector<int>(grid[0].size(),INT_MAX));
-//             bfd
+        int m = grid.size();
+        int n = grid[0].size();
         
-        for(int m = 0;m < grid.size();m++){
-            for(int n = 0; n< grid[0].size();n++){
-                if(grid[m][n] == 2){
-                    bfd(m,n,grid,level);
+        memset(level,0,sizeof(level));  
+        memset(vis,0,sizeof(vis));  
+        
+        queue <pair<pair<int,int>,int>> q;
+        // ((row,col),level);
+            
+        int cnt = 0;
+        
+        for(int i = 0; i<m;i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
                 }
+                else if(grid[i][j] ==1) cnt++;
             }
         }
+        
+        int dR[] = {1,-1,0,0};
+        int dC[] = {0,0,1,-1};
+        
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            
+            level[r][c] = t;
+            
+            for(int i = 0; i < 4;i++){
+                int Row = r+dR[i];
+                int Col = c+dC[i];
+                if(Row>=0 and Row < m and Col >=0 and Col < n and !vis[Row][Col] and grid[Row][Col] == 1){
+                     q.push({{Row,Col},t+1});
+                    cnt--;
+                    vis[Row][Col] =1;
+                }
+            }   
+        }
+        if(cnt != 0) return -1;
         int mx = 0;
-        for(int m = 0;m < grid.size();m++){
-            for(int n = 0; n< grid[0].size();n++){
-                
-                if(grid[m][n] != 0){
-                    if(level[m][n] == INT_MAX) return -1;
-                    mx = max(mx,level[m][n]);
+        
+        for(int i = 0; i<m;i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j]){
+                    mx = max(mx,level[i][j]);
                 }
             }
         }
         return mx;
-        
-    }
-    void bfd(int m ,int n,vector<vector<int>>& grid,vector<vector<int>> &level){
-        memset(vis,0,sizeof(vis));
-        queue<vector<int>> bfd;
-        bfd.push({m,n,0});
-        vis[m][n] = 1;
-        while(!bfd.empty()){
-            vector<int> temp = bfd.front();
-            // for()
-            bfd.pop();
-            int i = temp[0], j = temp[1],k = temp[2];
-            level[i][j] = min(k,level[i][j]);
-            
-            if(i+1< grid.size() and !vis[i+1][j] and grid[i+1][j]==1){ 
-                vis[i+1][j]=1;bfd.push({i+1,j,k+1});}
-            if(i-1>= 0 and !vis[i-1][j] and grid[i-1][j]==1){
-                vis[i-1][j]=1;bfd.push({i-1,j,k+1});}
-            if(j+1< grid[0].size() and !vis[i][j+1] and grid[i][j+1]==1){
-                vis[i][j+1]=1;bfd.push({i,j+1,k+1});}
-            if(j-1 >= 0 and !vis[i][j-1] and grid[i][j-1]==1){
-                vis[i][j-1] =1;bfd.push({i,j-1,k+1});}
-            
-                      
-        }
-        
     }
 };
