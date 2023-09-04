e2acd30686311a1905bfe090efc96dcd95d4efa6
Maheshkadam06 <92226051+Maheshkadam06@users.noreply.github.com>
2023-09-04 13:33:33 +0530

    Time: 154 ms, Memory: 104.8 MB - LeetHub

--- maximum-gap/maximum-gap.cpp
+++ maximum-gap/maximum-gap.cpp
@@ -1,38 +1,48 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
-        int mx = INT_MIN;
-        int mn = INT_MAX;
-        for(int i = 0; i< nums.size();i++){
-            mx = max(mx,nums[i]);
-            mn = min(mn, nums[i]);
+//     by radix sort
+        int mx = 0;
+        for(int i : nums){
+            mx = max(mx,i);
         }
-        int k = 100000;
-        double bucketsize = double((double)(mx-mn)/(double)k);
-        // cout<<bucketsize<<endl;
-        if(bucketsize<1) bucketsize =1.0;
-        vector<vector<int>> buckets(k);
-        for(int elmt : nums){
-            // cout<<elmt<<" ";
-            int index = (int)((double)(elmt-mn)/bucketsize);
-            // cout<<elmt<<" "<<index<<" ";
-            if(index == k) index = k-1;
-            buckets[index].push_back(elmt);
-        }
-        for(auto b: buckets){
-            sort(b.begin(),b.end());
+        int dec = 1;
+        vector<int> numscopy(nums.begin(),nums.end());
+        while(mx >= dec){
+            countingsort(numscopy,dec);
+            dec *= 10;
         }
         int gp = 0;
-        int last = INT_MAX;
-        for(auto b: buckets){
-            for(int i = 0; i < b.size();i++){
-                gp = max(gp,b[i]-last);
-                last = b[i];
-            }
-
+        for(int i = 1; i< nums.size();i++){
+            gp = max(gp,numscopy[i]-numscopy[i-1]);
+            // cout<<nums[i]<<" ";
         }
-        // int gp = 0;
         return gp;
     }
+    
+    void countingsort(vector<int> &nums,int dec){
+        int count[10] = {0};
+        for(int elmt : nums){
+            int dec_place = elmt/dec;
+            // cout<<dec_place<<" ";
+            count[dec_place%10]++;
+        }
+        // starting index
+        int carry = count[0];
+        count[0] = 0;
+        for(int i = 1; i < 10;i++){
+            int temp = count[i];
+            count[i] = count[i-1]+carry;
+            carry = temp;
+        }
+        // return;/
+        vector<int> unsorted(nums.begin(),nums.end());
+        for(int elmt : unsorted){
+            int dec_place = elmt/dec;
+            nums[count[dec_place%10]] = elmt;
+            count[dec_place%(10)]++;
+            
+        }
+    }
+    
 };
