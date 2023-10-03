536e512124122f3c5684394231270c452597a2f3
Maheshkadam06 <92226051+Maheshkadam06@users.noreply.github.com>
2023-10-03 10:46:02 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -9,34 +9,28 @@ using namespace std;
 
 class Solution{
 public:
-    int dp[500][500];
     int maximumPath(int N, vector<vector<int>> Matrix)
     {
         // code here
-        int mx = 0;
-        memset(dp,-1,sizeof(dp));
-        for(int c = 0; c < Matrix[0].size();c++){
-            mx = max(mx,drop(0,c,N,Matrix));
-        }
-        return mx;
-        
-    }
-    int drop(int r, int c, int n, vector<vector<int>>&mat){
-        if(r == n) return 0;
-        if(dp[r][c] != -1) return dp[r][c];
-        int dc[3] = {0,-1,1};
-        int mx = 0;
-        for(int i = 0; i < 3; i++){
-            int nr = r+1;
-            int nc = c+dc[i];
-            bool ingrid = nc >=0 and nc < mat[0].size() and nr < mat.size();
-            if(ingrid){
-                mx = max(mx,drop(nr,nc,n,mat));
+        vector<vector<int>> dp(2,vector<int>(Matrix[0].size(),0));
+        for(int r = 0; r <N;r++){
+            for(int c = 0; c<Matrix[0].size();c++){
+                int dc[3] = {0,-1,1};
+                for(int i = 0; i < 3; i++){
+                    int nc = c+dc[i];
+                    bool ingrid = nc >=0 and nc < Matrix[0].size();
+                    if(ingrid){
+                        dp[1][c] = max(dp[1][c], Matrix[r][c]+dp[0][nc]);
+                    }
+                }
             }
+            for(int c = 0; c<Matrix[0].size();c++) dp[0][c] = dp[1][c];
         }
-        // cout<<mx<<endl;
-        return dp[r][c] = mat[r][c]+mx;
-    }
+        
+        int mx = 0;
+        for(int c = 0; c<Matrix[0].size();c++) mx = max(mx,dp[0][c]);
+        return mx;
+        }
 };
 
 //{ Driver Code Starts.
